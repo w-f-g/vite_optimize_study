617415074a0747c7f73c2369cd334080ce035bbf
DESKTOP-O0776SD\wang <1035550175@qq.com>
2023-04-27 16:00:48 +0800

    优化了拆包逻辑

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from 'vite'
+import { UserConfig, defineConfig, loadEnv } from 'vite'
 import { ViteAliases } from 'vite-aliases'
 import react from '@vitejs/plugin-react'
 import { viteMockServe } from 'vite-plugin-mock'
@@ -6,6 +6,18 @@ import { compression } from 'vite-plugin-compression2'
 import { Plugin as vitePluginCDN } from 'vite-plugin-cdn-import'
 import { visualizer } from 'rollup-plugin-visualizer'
 import legacy from '@vitejs/plugin-legacy'
+import packageJSON from './package.json'
+
+// 不需要单独拆分的模块列表
+const exclusive: string[] = []
+// 需要拆包的模块列表
+const libs: Record<string, string> = Object.keys(packageJSON.dependencies)
+  .filter(name => !exclusive.includes(name))
+  .reduce((libs, name) => {
+    const key = `\/node_modules\/${name}\/`
+    libs[key] = name
+    return libs
+  }, {})
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => {
@@ -15,7 +27,7 @@ export default defineConfig(({mode}) => {
     plugins: [
       react(),
       legacy({
-        polyfills: ['es.global-this'],
+        polyfills: ['es.global-this', 'es.symbol'],
       }),
       ViteAliases({deep: false}),
       viteMockServe({
@@ -50,27 +62,22 @@ export default defineConfig(({mode}) => {
       rollupOptions: {
         output: {
           manualChunks(id: string) {
-            const chunksMap = {
-              "antd": "antd",
-              "axios": "axios",
-              "lodash-es": "lodash",
-              "echarts": "echarts",
-              "\/react\/": "react",
-              "\/react-dom\/": "react-dom",
-              "classnames": "classnames",
-              "styled-components": "styled-components",
-            }
-            let key = Object.keys(chunksMap).find(key => {
+            // console.log(id);
+            // 找到当前正在进行打包的模块
+            let key = Object.keys(libs).find(key => {
               return id.includes(key)
             })
-            if (key in chunksMap) {
-              return chunksMap[key]
+            if (key in libs) {
+              // 单独拆包
+              return libs[key]
             } else if (id.includes("node_modules")) {
+              // 其余的第三方模块合并打包进 vendor 文件
               return "vendor"
             }
+            // 剩下的业务模块交给 vite (准确来说是 rollup) 自行打包
           }
         }
       }
     }
-  }
+  } as UserConfig
 })
